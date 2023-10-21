96bec6e2e72792b257746fc8e13e4d5b911bdd9f
ALEXANDER ALBERTO SIGUENZA CAMPOS <alexander.siguenzac@claro.com.sv>
2023-10-21 12:39:30 -0600

    Se agrego splashscreen y loginsqlite

---
+++ app/src/main/java/edu/udb/splashscreenloginsqlitekotlin/WelcomeActivity.kt
@@ -0,0 +1,14 @@
+package edu.udb.splashscreenloginsqlitekotlin
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+        Toast.makeText(this@WelcomeActivity, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
+    }
+}
